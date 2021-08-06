b73bfc0134884ddfbdb85a3f5dcea4cb2f4f228a
lsinatra <83607794+lsinatra@users.noreply.github.com>
2021-08-06 20:41:29 +0700

    Update l_shaped_plots.py

--- Round A/l_shaped_plots.py
+++ Round A/l_shaped_plots.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2021 kamyu. All rights reserved.
+# Copyright (c) 2021 lsina. All rights reserved.
 #
 # Google Kick Start 2021 Round A - Problem B. L Shaped Plots
 # https://codingcompetitions.withgoogle.com/kickstart/round/0000000000436140/000000000068c509
