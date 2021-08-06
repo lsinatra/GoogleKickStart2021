8e342500b683981151b61384cbcf0dd4abb90ce1
lsinatra <83607794+lsinatra@users.noreply.github.com>
2021-08-06 20:41:04 +0700

    Update k_goodness_string.py

--- Round A/k_goodness_string.py
+++ Round A/k_goodness_string.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2021 kamyu. All rights reserved.
+# Copyright (c) 2021 lsina. All rights reserved.
 #
 # Google Kick Start 2021 Round A - Problem A. K-Goodness String
 # https://codingcompetitions.withgoogle.com/kickstart/round/0000000000436140/000000000068cca3
